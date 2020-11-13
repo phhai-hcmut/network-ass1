fd9430ed5c1c9dc55483827baaa01bf96fd980d7
Pham Hoang Hai <hai.pham.hoang@hcmut.edu.vn>
2020-11-13 23:07:03 +0700

    Add mjpeg_maker.py for create MJPEG video

---
+++ mjpeg_maker.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+import argparse
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('output_video')
+    parser.add_argument('input_images', nargs='+')
+    args = parser.parse_args()
+
+    with open(args.output_video, 'wb') as f:
+        for filename in args.input_images:
+            with open(filename, 'rb') as frame:
+                data = frame.read()
+                f.write("{:05d}".format(len(data)).encode())
+                # f.write(len(data).to_bytes(5, 'big'))
+                f.write(data)
