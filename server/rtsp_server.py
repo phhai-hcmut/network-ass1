a57d759318fb03973d7bfa04a0e8e2f3efd5ecf7
Pham Hoang Hai <hai.pham.hoang@hcmut.edu.vn>
2020-11-14 12:45:39 +0700

    Refactor server to allow switching videos

--- server/rtsp_server.py
+++ server/rtsp_server.py
@@ -1,6 +1,5 @@
 from enum import Enum
 import logging
-import os.path
 from random import randint
 import socket
 import threading
@@ -28,7 +27,7 @@ def start_server(listen_port, listen_addr=''):
         # Receive client info (address, port) through RTSP/TCP session
         worker_sock, client_addr_info = rtsp_socket.accept()
         logging.info("Accept new connection from %s:%d", *client_addr_info)
-        server_worker = ServerWorker(worker_sock, client_addr_info)
+        server_worker = ServerWorker(worker_sock)
         server_worker.start()
 
 
@@ -46,15 +45,19 @@ class RTSPResponse(Enum):
 class ServerWorker(threading.Thread):
     RTSP_VERSION = 'RTSP/1.0'
 
-    def __init__(self, socket, client_addr_info):
+    def __init__(self, socket):
         super().__init__()
         self.socket = socket
-        self.client_addr = client_addr_info
-        self.state = RTSPState.INIT
-        self.video_stream = None
-        self.session_id = None
-        self.rtp_sender = None
-        self.seqnum = None
+        self._state = RTSPState.INIT
+        self._video_stream = None
+        self._session_id = None
+        self._rtp_sender = None
+        self._seqnum = None
+
+    @property
+    def client_addr(self):
+        """The address of connected client"""
+        return self.socket.getpeername()
 
     def run(self):
         """Receive RTSP request from the client."""
@@ -62,31 +65,40 @@ class ServerWorker(threading.Thread):
             data = self.socket.recv(1024)
             if data:
                 logging.info("Data received:\n%s", data.decode())
-                self.process_rtsp_request(data)
+                self._process_rtsp_request(data)
             else:
                 # The client has closed connection
+                logging.info("Client %s:%d disconnected", *self.client_addr)
                 break
-        logging.info("Client %s:%d disconnected", *self.client_addr)
+        self._cleanup()
 
-    def process_rtsp_request(self, data):
+    def _process_rtsp_request(self, data):
         """Process RTSP request sent from the client."""
         # Get the request type
         request = data.decode().splitlines()
         status_line = request[0].split(' ')
         request_method = status_line[0]
 
-        # Get the RTSP sequence number
-        self.seqnum = request[1].split(' ')[1]
-
-        getattr(self, '_process_' + request_method.lower() + '_request')(request)
-
-    def _process_describe_request(self, request):
-        logging.info("Processing DESCRIBE request")
+        # Get the media file name
         filename = request[0].split(' ')[1]
+
+        def make_header(line):
+            header_line = line.split(' ')
+            header_name = header_line[0].strip(':')
+            return (header_name, ' '.join(header_line[1:]))
+        headers = dict(make_header(line) for line in request[1:])
+
+        # Get the RTSP sequence number
+        self._seqnum = int(headers['CSeq'])
+
+        getattr(self, '_process_' + request_method.lower() + '_request')(filename, headers)
+
+    def _process_describe_request(self, filename, headers):
+        logging.info("Processing DESCRIBE request")
         try:
             video_stream = VideoStream(filename)
         except FileNotFoundError:
-            self.reply_rtsp(RTSPResponse.FILE_NOT_FOUND)
+            self._reply_rtsp(RTSPResponse.FILE_NOT_FOUND)
             return
 
         body = '\n'.join([
@@ -101,7 +113,7 @@ class ServerWorker(threading.Thread):
             f'a=range:npt=0-{video_stream.duration}',
         ]).encode()
         headers = 'Content-Type: application/sdp'
-        self.reply_rtsp(RTSPResponse.OK, headers, body)
+        self._reply_rtsp(RTSPResponse.OK, headers, body)
         video_stream.close()
 
     @staticmethod
@@ -111,108 +123,87 @@ class ServerWorker(threading.Thread):
         timestamp = diff.days * 24 * 60 * 60 + diff.seconds
         return timestamp
 
-    def _process_setup_request(self, request):
+    def _process_setup_request(self, filename, headers):
         logging.info("Processing SETUP request")
-        if self.state == RTSPState.PLAYING:
+        if self._state == RTSPState.PLAYING:
             # We don't allow client to issue a SETUP request for a
             # stream that is already playing to change transport parameters
-            self.reply_rtsp(RTSPResponse.INVALID_METHOD)
-        else:
-            # Get the media file name
-            filename = request[0].split(' ')[1]
+            self._reply_rtsp(RTSPResponse.INVALID_METHOD)
+            return
 
-            # Get the RTP/UDP port from the last line
-            self.rtp_port = int(request[2].split(' ')[3])
+        # Get the RTP/UDP port from Transport header
+        rtp_port = int(headers['Transport'].split(' ')[2])
 
-            if self.video_stream is not None:
-                # Close old video_stream before open new one
-                self.video_stream.close()
-                self.video_stream = None
+        try:
+            video_stream = VideoStream(filename)
+        except FileNotFoundError:
+            self._reply_rtsp(RTSPResponse.FILE_NOT_FOUND)
+            return
 
+        if self._video_stream is not None:
+            # Close old video_stream before open new one
+            self._video_stream.close()
+        self._video_stream = video_stream
+
+        if self._rtp_sender is None:
+            rtp_addr = (self.client_addr[0], rtp_port)
             try:
-                video_stream = VideoStream(filename)
-            except FileNotFoundError:
-                self.reply_rtsp(RTSPResponse.FILE_NOT_FOUND)
+                rtp_sender = RTPSender(rtp_addr, self._video_stream)
+            except socket.error:
+                pass
             else:
-                self.video_stream = video_stream
+                self._rtp_sender = rtp_sender
+                # Create a new thread and start sending RTP packets
+                self._rtp_sender.start()
+        else:
+            # Send new video stream
+            self._rtp_sender.video_stream = self._video_stream
 
-                # Generate a randomized RTSP session ID
-                self.session_id = randint(100000, 999999)
+        # Generate a randomized RTSP session ID
+        self._session_id = randint(100000, 999999)
 
-                self.reply_rtsp(RTSPResponse.OK)
-                self.state = RTSPState.READY
+        self._reply_rtsp(RTSPResponse.OK)
+        self._state = RTSPState.READY
 
-    def _set_stream_time(self, request):
-        play_range = None
-        for line in request:
-            if line.startswith('Range:'):
-                play_range = line.partition(' ')[2]
-        if play_range:
-            begin, _ = _parse_npt(play_range)
-            self.video_stream.set_time(begin)
-
-    def _process_play_request(self, request):
+    def _process_play_request(self, filename, headers):
         logging.info("Processing PLAY request")
-        if self.state == RTSPState.INIT:
+        if self._state == RTSPState.INIT:
             # Client must call SETUP method before PLAY method
-            self.reply_rtsp(RTSPResponse.INVALID_METHOD)
+            self._reply_rtsp(RTSPResponse.INVALID_METHOD)
+            return
 
-        elif self.state == RTSPState.READY:
-            if self.rtp_sender is None:
-                try:
-                    rtp_addr = (self.client_addr[0], self.rtp_port)
-                    rtp_sender = RTPSender(rtp_addr, self.video_stream)
-                except socket.error:
-                    pass
-                else:
-                    self.rtp_sender = rtp_sender
-                    # Create a new thread and start sending RTP packets
-                    self.rtp_sender.start()
-            self._set_stream_time(request)
-            self.rtp_sender.play()
-            self.reply_rtsp(RTSPResponse.OK)
-            self.state = RTSPState.PLAYING
+        play_range = headers.get('Range', None)
+        if play_range is not None:
+            begin, _ = _parse_npt(play_range)
+            self._video_stream.set_time(begin)
 
-        elif self.state == RTSPState.PLAYING:
-            self._set_stream_time(request)
-            self.reply_rtsp(RTSPResponse.OK)
+        self._rtp_sender.play()
+        self._reply_rtsp(RTSPResponse.OK)
+        self._state = RTSPState.PLAYING
 
-    def _process_pause_request(self, request):
+    def _process_pause_request(self, filename, headers):
         logging.info("Processing PAUSE request")
-        if self.state == RTSPState.INIT:
-            self.reply_rtsp(RTSPResponse.INVALID_METHOD)
+        if self._state == RTSPState.INIT:
+            self._reply_rtsp(RTSPResponse.INVALID_METHOD)
+        else:
+            self._rtp_sender.pause()
+            self._reply_rtsp(RTSPResponse.OK)
+            self._state = RTSPState.READY
 
-        elif self.state == RTSPState.PLAYING:
-            self.rtp_sender.pause()
-            self.reply_rtsp(RTSPResponse.OK)
-            self.state = RTSPState.READY
-
-        elif self.state == RTSPState.READY:
-            self.reply_rtsp(RTSPResponse.OK)
-
-    def _process_teardown_request(self, request):
+    def _process_teardown_request(self, filename, headers):
         logging.info("Processing TEARDOWN request")
-        if self.video_stream is not None:
-            self.video_stream.close()
+        self._cleanup()
+        self._session_id = None
+        self._reply_rtsp(RTSPResponse.OK)
+        self._state = RTSPState.INIT
 
-        if self.rtp_sender is not None:
-            # Close the RTP sender
-            self.rtp_sender.close()
-            self.rtp_sender.join()
-
-        self.video_stream = None
-        self.session_id = None
-        self.rtp_sender = None
-        self.reply_rtsp(RTSPResponse.OK)
-        self.state = RTSPState.INIT
-
-    def reply_rtsp(self, resp, headers=None, body=None):
+    def _reply_rtsp(self, resp, headers=None, body=None):
         """Send RTSP reply to the client."""
         resp = resp.value
         reply = [
             f'{self.RTSP_VERSION} {resp}',  # status line
-            f'CSeq: {self.seqnum}',
-            f'Session: {self.session_id}',
+            f'CSeq: {self._seqnum}',
+            f'Session: {self._session_id}',
         ]
 
         if headers:
@@ -221,4 +212,14 @@ class ServerWorker(threading.Thread):
         if body:
             resp_msg += f'\nContent-Length: {len(body)}\n\n'.encode() + body
         sent_msg = self.socket.send(resp_msg)
-        logging.info(f"Sent {sent_msg} out of {len(resp_msg)} bytes")
+        logging.info("Sent %d out of %d bytes", sent_msg, len(resp_msg))
+
+    def _cleanup(self):
+        if self._video_stream is not None:
+            self._video_stream.close()
+            self._video_stream = None
+
+        if self._rtp_sender is not None:
+            self._rtp_sender.close()
+            self._rtp_sender.join()
+            self._rtp_sender = None
