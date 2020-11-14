9aa067384445e9bdde20727f7bc6c86b576e3624
Long Trần Hoàng <bigtimegangstar@gmail.com>
2020-11-14 15:03:12 +0700

    add option to show normal or simple gui in args

--- client/__main__.py
+++ client/__main__.py
@@ -1,7 +1,7 @@
 import argparse
 import logging
 
-from .gui import Client
+from .gui import Client,Client2
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -9,13 +9,19 @@ if __name__ == '__main__':
     parser.add_argument('server_port', type=int)
     parser.add_argument('rtp_port', type=int)
     parser.add_argument('file_name')
+    parser.add_argument('-simple',action='store_true',help='use simple GUI')
+    
     args = parser.parse_args()
+    simple = args.simple
+
 
     logging.basicConfig(
         level=logging.DEBUG, format="%(asctime)s:%(levelname)s:%(message)s"
     )
 
     # Create a new client
-    app = Client(**vars(args))
+    arg_dict = vars(args)
+    arg_dict.pop('simple')
+    app = Client2(**arg_dict) if simple else Client(**arg_dict)
     app.title("RTPClient")
     app.mainloop()
