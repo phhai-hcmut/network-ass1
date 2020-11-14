f8350e78423487b169ca90d062ece2224d6210c6
Pham Hoang Hai <hai.pham.hoang@hcmut.edu.vn>
2020-11-14 11:22:39 +0700

    Add display remaining time to GUI

--- client/gui.py
+++ client/gui.py
@@ -15,9 +15,6 @@ def _parse_npt(string):
     return float(begin), float(end)
 
 
-FRAME_RATE = 20
-
-
 class Client(tk.Tk):
     """GUI interface for RTSP client"""
     # CONTROL_BUTTONS = ["Describe", "Set up", "Play", "Pause", "Tear down"]
@@ -28,12 +25,13 @@ class Client(tk.Tk):
     def __init__(self, server_addr, server_port, rtp_port, file_name):
         super().__init__()
         self.protocol('WM_DELETE_WINDOW', self.teardown)
-        self.rtsp_client = RTSPClient((server_addr, server_port))
+        self._rtsp_client = RTSPClient((server_addr, server_port))
         self.rtp_port = rtp_port
         self.file_name = file_name
-        self.rtp_recv = None
-        self._current_progress = 0
+        self._rtp_recv = None
+        self._video_info = {}
         self.create_widgets()
+        self._get_video_info()
 
     def create_widgets(self):
         placeholer_img = ImageTk.BitmapImage(Image.new('1', (384, 288)))
@@ -63,8 +61,29 @@ class Client(tk.Tk):
             )
             button.grid(row = 3, column=i)
 
+        self._video_duration = tk.StringVar()
+        label = tk.Label(self, textvariable=self._video_duration, background='white')
+        label.grid(row=1, column=3)
+
+        self._video_remain = tk.StringVar()
+        label = tk.Label(self, textvariable=self._video_remain, background='white')
+        label.grid(row=1, column=0)
+
+    def _get_video_info(self):
+        self._video_info['progress'] = 0
+        message = self._rtsp_client.describe(self.file_name)
+        # Get total duration of the video
+        range_line = next(line for line in message if line.startswith('a=range'))
+        _, video_duration = _parse_npt(range_line.removeprefix('a=range:'))
+        self._video_duration.set(f"Duration: {video_duration}")
+        self._video_remain.set(f"Remaining: {video_duration:03}")
+        self._video_info['duration'] = video_duration
+
+        framerate_line = next(line for line in message if line.startswith('a=framerate'))
+        self._video_info['frame_rate'] = float(framerate_line.removeprefix('a=framerate:'))
+
     def describe(self):
-        message = "\n".join(self.rtsp_client.describe(self.file_name))
+        message = "\n".join(self._rtsp_client.describe(self.file_name))
         if message:
             describe_frame = tk.Label(
                 self, text=message, background='white',
@@ -73,43 +92,36 @@ class Client(tk.Tk):
             describe_frame.grid(row=4, column=0, columnspan=len(self.PLAYBACK_BUTTONS))
 
     def setup(self):
-        message = self.rtsp_client.describe(self.file_name)
-        # Get total duration of the video
-        range_line = next(line for line in message if line.startswith('a=range'))
-        _, self._video_duration = _parse_npt(range_line.removeprefix('a=range:'))
-        label = tk.Label(self, text='Duration:'+str(self._video_duration), background='white')
-        label.grid(row=3,column = 3)
-        self.rtsp_client.setup(self.file_name, self.rtp_port)
-        self.rtp_recv = RTPReceiver(self.rtp_port)
+        self._rtsp_client.setup(self.file_name, self.rtp_port)
+        self._rtp_recv = RTPReceiver(self.rtp_port)
 
     def play(self,jump = False):
         if jump:
-            self.rtsp_client.play(self._current_progress)
+            self._rtsp_client.play(self._video_info['progress'])
         else:
-            self.rtsp_client.play()
+            self._rtsp_client.play()
 
         while True:
-            video_data = self.rtp_recv.read()
+            video_data = self._rtp_recv.read()
             if video_data:
                 self.show_jpeg(video_data)
             else:
                 break
 
     def pause(self):
-        self.rtsp_client.pause()
+        self._rtsp_client.pause()
 
     def teardown(self):
-        is_playing = self.rtsp_client.state == RTSPState.PLAYING
+        is_playing = self._rtsp_client.state == RTSPState.PLAYING
         if is_playing:
             self.pause()
 
         if tk.messagebox.askokcancel("Quit?", "Are you sure you want to quit?"):
-            if self.rtsp_client.state != RTSPState.INIT:
-                self.rtsp_client.teardown()
+            if self._rtsp_client.state != RTSPState.INIT:
+                self._rtsp_client.teardown()
                 self.cleanup()
-                self.destroy()
-            else:
-                self.destroy()
+            self.destroy()
+            return
 
         if is_playing: self.play()
 
@@ -120,19 +132,21 @@ class Client(tk.Tk):
         # Keep a reference to the image object
         self.image_frame.update()
         self.image_frame.image = self.image
-        self._current_progress += 1 / FRAME_RATE
+        self._video_info['progress'] += 1 / self._video_info['frame_rate']
+        remain = self._video_info['duration'] - self._video_info['progress']
+        self._video_remain.set(f"Remaining: {round(remain)}")
         # if self.is_playing:
         #     self.after(round(1000 / FRAME_RATE), self.show_jpeg)
 
     def forward(self):
-        self._current_progress += 5
+        self._video_info['progress'] += 5
         self.play(True)
 
     def backward(self):
-        if self._current_progress > 5:
-            self._current_progress -= 5
+        if self._video_info['progress'] > 5:
+            self._video_info['progress'] -= 5
         else:
-            self._current_progress = 0
+            self._video_info['progress'] = 0
         self.play(True)
 
 
@@ -142,9 +156,9 @@ class Client(tk.Tk):
 
     def cleanup(self):
         logging.info("Cleaning resources before exiting application...")
-        self.rtsp_client.close()
-        if self.rtp_recv is not None:
-            self.rtp_recv.close()
+        self._rtsp_client.close()
+        if self._rtp_recv is not None:
+            self._rtp_recv.close()
 
 
 class Client2(Client):
